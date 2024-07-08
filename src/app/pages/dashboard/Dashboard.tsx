444de4b01b049258607b31fbcce81be13dc2bbac
msilvalcs <lcsmssilvabh@gmail.com>
2024-07-08 12:57:49 -0300

    feat: add functions w/ useState, useEffect, useMemo and useRef

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,19 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
+  const counterRef = useRef({ counter: 0 });
+  return (
+    <div>
+      <Link to={"/login"}>Login</Link>
+      <p> Dashboard </p>
 
-    return(
-        <div>
-            <p> Dashboard </p>
-            <Link to={"/login"}>Login</Link>
-        </div>
-    );
-}
+      <p>Contador: {counterRef.current.counter} </p>
+
+      <button onClick={() => counterRef.current.counter++}>Somar</button>
+      <button onClick={() => console.log(counterRef.current.counter)}>
+        Log
+      </button>
+    </div>
+  );
+};
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,35 +1,78 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleEntrar = () =>{
-        console.log(email)
-        console.log(password)
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+  const buttonEntrar = useRef<HTMLButtonElement>(null);
+
+  /* FUNÇÃO PARA CONTROLAR QUANDO BLOCO DE CÓDIGO SERÁ EXECUTADO
+    
+   useEffect(() => {
+    if (window.confirm('Você é homem?')) {
+        console.log('Homem')
+    } else {
+        console.log('Mulher')
     }
+   }, []);
 
-    return(
-        <div>
-            {/* Formulário para logar */}
-            <form>
-                
-                <label>
-                    <span>E-mail:</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)} />
-                </label>
-                <input />
-        
-                <label>
-                    <span>Senha:</span>
-                    <input type="password" value={password} onChange={e => setEmail(e.target.value)}/>
-                </label>
+   useEffect(() => {
+        console.log(email)
+    }, [email]);
 
-                <button type="button" onClick={handleEntrar}>
-                    Entrar
-                </button>
+    useEffect(() => {
+        console.log(password)
+    }, [password]); */
 
-            </form>
-        </div>
-    );
-}
+  /* FUNÇÃO PARA REALIZAR UMA OPERAÇÃO */
+  const emailLength = useMemo(() => {
+    console.log("Executou");
+    return email.length * 1000;
+  }, [email.length]);
+
+  /* FUNÇÃO QUE NÃO SERÁ RECONSTRUÍDA TODA VEZ QUE UM COMPONENT FOR ALTERADO */
+  const handleEntrar = useCallback(() => {
+    console.log(email);
+    console.log(password);
+  }, [email, password]);
+
+  return (
+    <div>
+      {/* Formulário para logar */}
+      <form>
+        <Link to={"/dasboard"}>Tela inicial</Link>
+        <p>Quantidade de caracteres no email: {emailLength} </p>
+
+        <label>
+          <span>E-mail:</span>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" ? inputPasswordRef.current?.focus() : undefined
+            }
+          />
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            value={password}
+            ref={inputPasswordRef}
+            onChange={(e) => setPassword(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" ? buttonEntrar.current?.focus() : undefined
+            }
+          />
+        </label>
+
+        <button type="button" onClick={handleEntrar} ref={buttonEntrar}>
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+};
